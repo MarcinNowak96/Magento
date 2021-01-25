4839bd6aba0dfa9c9b7e4bc14bf1977df27b3449
MarcinNowak96 <marcineknowak1996@gmail.com>
2021-01-25 10:05:03 +0100

    Module to change the home page

---
+++ app/code/SimpleField/HomeModule/Block/Html/NewBlock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Magento\Theme\Block\Html;
+//tutaj nie mogłem ogarnąć, czy wpisać namespace taki jak jest czyli SimpleField\HomeModule\BLock\Html, czy właśnie taki jaki jaki wpisałem
+
+use Magento\Framework\View\Element\Template;
+
+class NewBlock extends Template
+{
+    //tu  miałem prostą funkcje getHello(), która tylko wyświtlała echo "hello"
+}
