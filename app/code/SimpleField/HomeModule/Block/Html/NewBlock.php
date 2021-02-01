e5445c4cf1760f2b209fe65c381acfa03c98b756
MarcinNowak96 <marcineknowak1996@gmail.com>
2021-02-01 11:43:56 +0100

    test css

--- app/code/SimpleField/HomeModule/Block/Html/NewBlock.php
+++ app/code/SimpleField/HomeModule/Block/Html/NewBlock.php
@@ -14,19 +14,29 @@ class NewBlock extends Template
      */
 
     protected $_template = 'SimpleField_HomeModule::html/newBlock.phtml';
+    protected $cssClass;
 
     public function __construct(Template\Context $context, array $data = [])
     {
         parent::__construct($context, $data);
+        $this->cssClass = $data['css_class'];
     }
+
+    public function getCssClass()
+    {
+        return $this->cssClass;
+    }
+
     public function getKostenloser(): string
     {
         return "KOSTENLOSER VERSAND";
     }
+
     public function getExpress(): string
     {
         return "EXPRESSVERSAND";
     }
+
     public function getNumber(): string
     {
         return "MO-FR: +49938747189";
